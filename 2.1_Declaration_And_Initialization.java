faf5b947f0c0d31934d6cd73f053181732a1e409
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:11:31 +0530

    Add files via upload

---
+++ 2.1_Declaration_And_Initialization.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Declaration_And_Initialization
+{
+
+	public static void main(String[] args)
+	{
+		
+		int num[]= {11,22,33,44,55};
+		System.out.println("Result= "+num[4]);
+
+	}
+
+}
