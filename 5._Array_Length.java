e6bf1bea98c0fa356c3cedd80a0c75475775bcfe
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:13:05 +0530

    Add files via upload

---
+++ 5._Array_Length.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Array_Length
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int num[]=new int[5];
+		int a=num.length;
+		System.out.println("Array Length = "+a);
+
+	}
+
+}
