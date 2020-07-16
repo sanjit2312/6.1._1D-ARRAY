bd1957623d6e2185bcbef4aff4945f848550bed0
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:11:07 +0530

    Add files via upload

---
+++ 1._Initialization.java
@@ -0,0 +1,20 @@
+package array;
+
+public class Initialization 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int num[]=new int[5];
+		num[0]=11;
+		num[1]=22;
+		num[2]=33;
+		num[3]=44;
+		num[4]=55;
+		System.out.println("Result= "+num[3]);
+		
+
+	}
+
+}
